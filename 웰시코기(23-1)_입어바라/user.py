2eca19b7bd33eab71d1800e2dee4233b2adc3eaf
Woosung Jo <115052294+wooooooooooooooosung@users.noreply.github.com>
2023-05-29 22:50:08 +0900

    Update user.py

--- 웰시코기(23-1)_입어바라/user.py
+++ 웰시코기(23-1)_입어바라/user.py
@@ -2,6 +2,7 @@ import db
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QDate
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 from PyQt5 import *
@@ -16,7 +17,8 @@ class userWindow(QMainWindow, uic.loadUiType("./ui/user.ui")[0]):
         self.gridLayout.addWidget(self.webEngineView)
         self.webEngineView.load(QUrl("https://naver.me/FOvSy6I3"))
 
-        info = db.executeQuery("SELECT * FROM USER WHERE userID = 1")[0]
-        self.name.setText(info[1])
-        self.sex.setText(info[2])
-        self.address.setText(info[4])
+        list = db.executeQuery("SELECT * FROM USER WHERE userID = 1")[0]
+        self.name.setText(list[1])
+        self.birth.setDate(QDate.fromString(list[2], "yyyy-MM-dd"))
+        self.sex.setText(list[3])
+        self.address.setText(list[4])
