a4a5f8f97665ab7a586d9e98e1e99535fc060988
승진박 <evan@lsware.co.kr>
2023-05-30 17:35:01 +0900

    mod: image 다중 로드 기능 추가 완료

--- 웰시코기(23-1)_입어바라/board.py
+++ 웰시코기(23-1)_입어바라/board.py
@@ -17,18 +17,26 @@ class boardWindow(QMainWindow, uic.loadUiType("./ui/board.ui")[0]):
         #print(len(list), list)
 
         self.print_posts(list)
+        self.show()
 
     def print_posts(self, list):
+        x = 20
+        y = 60
+        image_size = 230
+
         for idx, row in enumerate(list):
             #print(idx, row[0], row[1])
 
             image_path = './img/' + row[1] + '.jpg'
-            pixmap = QtGui.QPixmap(image_path).scaled(150,150)
+            pixmap = QtGui.QPixmap(image_path).scaled(image_size, image_size)
             label = QLabel(self)
             label.setPixmap(pixmap)
-            label.setGeometry(150*idx, 150*idx, 150, 150)
+            label.setGeometry(x, y, image_size, image_size)
+
+            #if (x + image_size) > 250:
+
             label.mousePressEvent = lambda event, image_id=row[0]: self.imgBtn_click(image_id)
-            self.show()
+            label.show()
 
     def imgBtn_click(self, postID) :
         self.trade = tradeWindow(postID)
