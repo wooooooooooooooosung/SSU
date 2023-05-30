a31e2b290352c8b7858ae5073cc60fd2361f2260
AlcoholGame <43295597+alcoholgame@users.noreply.github.com>
2023-05-30 20:11:12 +0900

    mod: board 1차 개발 완성

--- 웰시코기(23-1)_입어바라/board.py
+++ 웰시코기(23-1)_입어바라/board.py
@@ -1,43 +1,61 @@
 import db
-
 from trade import tradeWindow
-
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QDate
-from PyQt5.QtCore import QUrl
-from PyQt5 import *
+from PyQt5.QtCore import QDate, Qt
+from PyQt5.QtGui import QPixmap
 
 class boardWindow(QMainWindow, uic.loadUiType("./ui/board.ui")[0]):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        list = db.executeQuery("SELECT postID, postName FROM Post")
-        #print(len(list), list)
+        list_data = db.executeQuery("SELECT postID, postName FROM Post")
+        self.print_posts(list_data)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setGeometry(10, 50, 480, 460)
 
-        self.print_posts(list)
         self.show()
 
-    def print_posts(self, list):
-        x = 20
-        y = 60
-        image_size = 230
+    def print_posts(self, list_data):
+        image_size = 220
+        images_per_row = 2
 
-        for idx, row in enumerate(list):
-            #print(idx, row[0], row[1])
+        scroll_content = QWidget(self.scrollArea)
+        scroll_layout = QGridLayout(scroll_content)
+        scroll_layout.setHorizontalSpacing(10)
+        scroll_layout.setVerticalSpacing(5)
 
-            image_path = './img/' + row[1] + '.jpg'
-            pixmap = QtGui.QPixmap(image_path).scaled(image_size, image_size)
-            label = QLabel(self)
-            label.setPixmap(pixmap)
-            label.setGeometry(x, y, image_size, image_size)
+        row = 0
+        column = 0
 
-            #if (x + image_size) > 250:
+        for idx, row_data in enumerate(list_data):
+            image_path = './img/' + row_data[1] + '.jpg'
+            pixmap = QPixmap(image_path).scaled(image_size, image_size)
 
-            label.mousePressEvent = lambda event, image_id=row[0]: self.imgBtn_click(image_id)
-            label.show()
+            image_label = QLabel(scroll_content)
+            image_label.setPixmap(pixmap)
+            image_label.setAlignment(Qt.AlignCenter)
 
-    def imgBtn_click(self, postID) :
+            text_label = QLabel(row_data[1], scroll_content)
+            text_label.setAlignment(Qt.AlignCenter)
+            text_label.setStyleSheet("color: white;")
+
+            scroll_layout.addWidget(image_label, row, column)
+            scroll_layout.addWidget(text_label, row + 1, column)
+
+            image_label.mousePressEvent = lambda event, image_id=row_data[0]: self.imgBtn_click(image_id)
+
+            column += 1
+            if column >= images_per_row:
+                row += 2 
+                column = 0
+
+        scroll_layout.setRowStretch(row, 1)  
+
+        self.scrollArea.setWidget(scroll_content)
+
+    def imgBtn_click(self, postID):
         self.trade = tradeWindow(postID)
         self.trade.show()
