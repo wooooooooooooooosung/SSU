6c09c5857570abb95d0f98682807a174d39655f1
Woosung Jo <115052294+wooooooooooooooosung@users.noreply.github.com>
2023-07-31 22:23:42 +0900

    Create map.py

---
+++ map.py
@@ -0,0 +1,41 @@
+import requests
+
+
+def getLatLng(address):
+    result = ""
+
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address
+    rest_api_key = '758707f6a274ebc390627c283aa67caa'
+    header = {'Authorization': 'KakaoAK ' + rest_api_key}
+
+    r = requests.get(url, headers=header)
+
+    if r.status_code == 200:
+        result_address = r.json()["documents"][0]["address"]
+        
+        result = result_address["y"], result_address["x"]
+    else:
+        result = "ERROR[" + str(r.status_code) + "]"
+    return result
+def getKakaoMapHtml(address_latlng):
+    javascript_key = "24c90bc08df574c851cbff398327ac87"
+
+    result = ""
+    result = result + "<div id='map' style='width:475px;height:245px;display:inline-block;'></div>" + "\n"
+    result = result + "<script type='text/javascript' src='//dapi.kakao.com/v2/maps/sdk.js?appkey=" + javascript_key + "'></script>" + "\n"
+    result = result + "<script>" + "\n"
+    result = result + "    var container = document.getElementById('map'); " + "\n"
+    result = result + "    var options = {" + "\n"
+    result = result + "           center: new kakao.maps.LatLng(" + address_latlng[0] + ", " + address_latlng[1] + ")," + "\n"
+    result = result + "           level: 3" + "\n"
+    result = result + "    }; " + "\n"
+    result = result + "    var map = new kakao.maps.Map(container, options); " + "\n"
+    
+    # 검색한 좌표의 마커 생성을 위해 추가
+    result = result + "    var markerPosition  = new kakao.maps.LatLng(" + address_latlng[0] + ", " + address_latlng[1] + ");  " + "\n"
+    result = result + "    var marker = new kakao.maps.Marker({position: markerPosition}); " + "\n"
+    result = result + "    marker.setMap(map); " + "\n"
+
+    result = result + "</script>" + "\n"
+    
+    return result
